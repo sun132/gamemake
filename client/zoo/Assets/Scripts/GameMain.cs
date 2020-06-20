ce7a2ef5864102d3004acf1ebabe4dde2d0e687b
guo122 <balanuard@163.com>
2020-06-21 00:14:23 +0800

    [Client] first commit unity project.

---
+++ client/zoo/Assets/Scripts/GameMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameMain : MonoBehaviour
+{
+    public Transform transformCamera;
+
+    void Start()
+    {
+        DOTween.Sequence().Append(transformCamera.DOMoveX(88f, 3f))
+            .Append(transformCamera.DOMoveX(0f, 5f))
+            .SetLoops(-1);
+    }
+}
